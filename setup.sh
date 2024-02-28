0f495711d0c1626b3d499165a3594fbc68247178
Darko Vukovic <vdarkobar@gmail.com>
2024-02-28 09:56:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -77,7 +77,6 @@ fi
 #######################
 # Create backup files #
 #######################
-
 echo
 echo -e "${GREEN} Creating backup files ${NC}"
 
@@ -183,68 +182,6 @@ else
 fi
 
 
-#############################
-# Modify dhclient.conf file #
-#############################
-echo
-echo -e "${GREEN}Preventing${NC} dhclient ${GREEN}from overwriting${NC} resolve.conf"
-
-sleep 0.5 # delay for 0.5 seconds
-echo
-
-# Path to the dhclient.conf file
-DHCLIENT_CONF="/etc/dhcp/dhclient.conf"
-
-# Check if the dhclient.conf file exists
-if [ ! -f "$DHCLIENT_CONF" ]; then
-    echo -e "${RED}Error: $DHCLIENT_CONF does not exist. ${NC}"
-    exit 1
-fi
-
-# Backup the original file before making changes
-sudo cp $DHCLIENT_CONF "${DHCLIENT_CONF}.bak"
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error: Failed to backup the original dhclient.conf file. ${NC}"
-    exit 1
-fi
-
-# Replace the specified lines
-sudo sed -i 's/domain-name, domain-name-servers, domain-search, host-name,/domain-name, domain-search, host-name,/' $DHCLIENT_CONF
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error: Failed to replace the first specified line. ${NC}"
-    exit 1
-fi
-
-sudo sed -i 's/dhcp6.name-servers, dhcp6.domain-search, dhcp6.fqdn, dhcp6.sntp-servers,/dhcp6.domain-search, dhcp6.fqdn, dhcp6.sntp-servers,/' $DHCLIENT_CONF
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error: Failed to replace the second specified line. ${NC}"
-    exit 1
-fi
-
-# Get the primary IP address of the machine
-IP_ADDRESS=$(hostname -I | awk '{print $1}')
-if [ -z "$IP_ADDRESS" ]; then
-    echo -e "${RED}Error: Failed to obtain the IP address of the machine. ${NC}"
-    exit 1
-fi
-
-# Check and replace the "prepend domain-name-servers" line with the machine's IP address
-sudo sed -i "/^#prepend domain-name-servers 127.0.0.1;/a prepend domain-name-servers ${IP_ADDRESS};" $DHCLIENT_CONF
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error: Failed to insert the machine's IP address. ${NC}"
-    exit 1
-fi
-
-# Now, find the line with the machine's IP address and add the 127.0.0.1 below it
-sudo sed -i "/^prepend domain-name-servers ${IP_ADDRESS};/a prepend domain-name-servers 127.0.0.1;" $DHCLIENT_CONF
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error: Failed to insert the 127.0.0.1 address below the machine's IP address. ${NC}"
-    exit 1
-fi
-
-echo -e "${GREEN}Modifications completed successfully. ${NC}"
-
-
 ####################
 # Prepare firewall #
 ####################
@@ -262,7 +199,6 @@ echo
 ######################################
 # Set User/Group/Folders/Premissions #
 ######################################
-
 # Create directories
 if ! sudo mkdir -p /public || ! sudo mkdir -p /private; then
     echo -e "${RED}Error: Failed to create directories. ${NC}"
@@ -422,7 +358,6 @@ fi
 ######################
 # Info before reboot #
 ######################
-
 echo -e "${GREEN}REMEMBER: ${NC}"
 sleep 0.5 # delay for 0.5 seconds
 echo
@@ -457,7 +392,6 @@ echo
 ##########################
 # Prompt user for reboot #
 ##########################
-
 while true; do
     read -p "Do you want to reboot the server now (recommended)? (yes/no): " response
     case "${response,,}" in
@@ -466,3 +400,13 @@ while true; do
         *) echo -e "${YELLOW}Invalid response. Please answer${NC} yes or no." ;;
     esac
 done
+
+
+########################################
+# Remove Script(s) from the system #
+########################################
+echo
+echo -e "${RED}This Script Will Self Destruct!${NC}"
+echo
+# VERY LAST LINE OF THE SCRIPT:
+rm -- "$0"
